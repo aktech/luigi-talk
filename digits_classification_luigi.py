ea5d2241c57363a496ef2679c7e6e3c80a869819
Amit Kumar <dtu.amit@gmail.com>
2017-11-07 14:38:29 +0000

    Luigify tradition stuff

--- digits_classification_luigi.py
+++ digits_classification_luigi.py
@@ -12,6 +12,7 @@ from sklearn import svm
 IMAGES_AND_LABELS_HDF5_FILE = 'digit_clf_images_and_labels.hdf5'
 RESHAPED_IMAGES_AND_LABELS_HDF5_FILE = 'digit_clf_reshaped_images_and_labels.hdf5'
 CLASSIFIER_PKL = 'digit_clf_classifier.pkl'
+CONFUSION_MATRIX_PKL = 'digit_clf_confusion_matrix.pkl'
 IMAGES_H5_KEY = 'images'
 LABELS_H5_KEY = 'labels'
 
@@ -19,53 +20,75 @@ LABELS_H5_KEY = 'labels'
 class GetImagesDatasetTask(luigi.Task):
 
     def run(self):
-        pass
+        logging.info('Getting Dataset')
+        digits = datasets.load_digits()
+        images_and_labels_h5 = h5py.File(self.output().path, 'w')
+        images_and_labels_h5.create_dataset(name='images', data=digits.images)
+        images_and_labels_h5.create_dataset(name='labels', data=digits.target)
 
     def output(self):
-        luigi.LocalTartget()
-
-def get_images_dataset():
-    logging.info('Getting Dataset')
-    digits = datasets.load_digits()
-    images_and_labels_h5 = h5py.File(IMAGES_AND_LABELS_HDF5_FILE, 'w')
-    images_and_labels_h5.create_dataset(name='images', data=digits.images)
-    images_and_labels_h5.create_dataset(name='labels', data=digits.target)
+        return luigi.LocalTarget(IMAGES_AND_LABELS_HDF5_FILE)
 
 
-def preprocess_images():
-    logging.info('Pre-processing images')
-    hf = h5py.File(IMAGES_AND_LABELS_HDF5_FILE, 'r')
-    images, labels = hf[IMAGES_H5_KEY].value, hf[LABELS_H5_KEY].value
-    reshaped_images = images.reshape((len(images), -1))
-    reshaped_images_and_labels_h5 = h5py.File(RESHAPED_IMAGES_AND_LABELS_HDF5_FILE, 'w')
-    reshaped_images_and_labels_h5.create_dataset(name=IMAGES_H5_KEY, data=reshaped_images)
-    reshaped_images_and_labels_h5.create_dataset(name=LABELS_H5_KEY, data=labels)
+class PreprocessImagesTask(luigi.Task):
+
+    def requires(self):
+        return GetImagesDatasetTask()
+
+    def output(self):
+        return luigi.LocalTarget(RESHAPED_IMAGES_AND_LABELS_HDF5_FILE)
+
+    def run(self):
+        logging.info('Pre-processing images')
+        hf = h5py.File(self.requires().output().path, 'r')
+        images, labels = hf[IMAGES_H5_KEY].value, hf[LABELS_H5_KEY].value
+        reshaped_images = images.reshape((len(images), -1))
+        reshaped_images_and_labels_h5 = h5py.File(self.output().path, 'w')
+        reshaped_images_and_labels_h5.create_dataset(name=IMAGES_H5_KEY, data=reshaped_images)
+        reshaped_images_and_labels_h5.create_dataset(name=LABELS_H5_KEY, data=labels)
 
 
-def train_classifier():
-    logging.info('Training Classifier')
-    hf = h5py.File(RESHAPED_IMAGES_AND_LABELS_HDF5_FILE, 'r')
-    images, target = hf[IMAGES_H5_KEY].value, hf[LABELS_H5_KEY].value
-    n_samples = len(images)
-    classifier = svm.SVC(gamma=0.001)
-    # We learn the digits on the first half of the digits
-    classifier.fit(images[:n_samples // 2], target[:n_samples // 2])
-    pd.to_pickle(classifier, CLASSIFIER_PKL)
+class TrainingClassifierTask(luigi.Task):
+
+    def requires(self):
+        return PreprocessImagesTask()
+
+    def output(self):
+        return luigi.LocalTarget(CLASSIFIER_PKL)
+
+    def run(self):
+        logging.info('Training Classifier')
+        hf = h5py.File(self.requires().output().path, 'r')
+        images, target = hf[IMAGES_H5_KEY].value, hf[LABELS_H5_KEY].value
+        n_samples = len(images)
+        classifier = svm.SVC(gamma=0.001)
+        # We learn the digits on the first half of the digits
+        classifier.fit(images[:n_samples // 2], target[:n_samples // 2])
+        pd.to_pickle(classifier, self.output().path)
 
 
-def predict():
-    logging.info('Predicting Images')
-    hf = h5py.File(RESHAPED_IMAGES_AND_LABELS_HDF5_FILE, 'r')
-    images, target = hf['images'].value, hf['labels'].value
-    n_samples = len(images)
-    classifier = pd.read_pickle(CLASSIFIER_PKL)
-    expected = target[n_samples // 2:]
-    predicted = classifier.predict(images[n_samples // 2:])
-    confusion_matrix = metrics.confusion_matrix(expected, predicted)
-    logging.info("Classification report for classifier %s:\n%s\n"
-                 % (classifier, metrics.classification_report(expected, predicted)))
-    logging.info("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
-    return confusion_matrix
+class PredictionTask(luigi.Task):
+
+    def requires(self):
+        return TrainingClassifierTask()
+
+    def output(self):
+        return luigi.LocalTarget(CONFUSION_MATRIX_PKL)
+
+    def run(self):
+        logging.info('Predicting Images')
+        hf = h5py.File(RESHAPED_IMAGES_AND_LABELS_HDF5_FILE, 'r')
+        images, target = hf['images'].value, hf['labels'].value
+        n_samples = len(images)
+        classifier = pd.read_pickle(CLASSIFIER_PKL)
+        expected = target[n_samples // 2:]
+        predicted = classifier.predict(images[n_samples // 2:])
+        confusion_matrix = metrics.confusion_matrix(expected, predicted)
+        logging.info("Classification report for classifier %s:\n%s\n"
+                     % (classifier, metrics.classification_report(expected, predicted)))
+        logging.info("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
+        pd.to_pickle(confusion_matrix, CONFUSION_MATRIX_PKL)
+        logging.info(confusion_matrix)
 
 
 def setup_logging():
@@ -74,7 +97,4 @@ def setup_logging():
 
 if __name__ == '__main__':
     setup_logging()
-    get_images_dataset()
-    preprocess_images()
-    train_classifier()
-    predict()
+    luigi.run()
